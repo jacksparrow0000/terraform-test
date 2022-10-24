0afacdb20b1efc06bd401ae23636676f59da1ff3
jack <gauravkumarsinha@gmail.com>
2022-10-24 05:41:30 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-  bucket = "ua-terraform-state"
+  bucket = "dev-terraform-state-gks"
   key = "dev/ec2-app2/terraform.tfstate"
-  region = "us-east-2"
+  region = "us-east-1"
   }
 }
